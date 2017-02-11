80867b6636fedb7b3c5380a823b1d6dc1182499e
amit <shettya@gmail.com>
2017-02-11 07:50:14 -0800

    set up app folder structure. create route. create sass file and link eveything

---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+module.exports = function(app){
+
+	app.get('/', function(req,res){
+
+		res.sendFile(process.cwd() + '/public/index.html');
+
+	});
+}
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 'use strict';
 
 let express = require('express');
+let routes = require(__dirname + '/app/routes/index.js');
 let app = express();
 
 app.set('port', (process.env.PORT || 3000));
+app.use('/css', express.static(__dirname + '/public/css'));
 
-app.get('/', function(req,res){
-
-
-});
+routes(app);
 
 app.listen(app.get('port'), function(){
 	console.log(`Now listening on port ${app.get('port')}`);
