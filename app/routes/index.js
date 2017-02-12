7fca85b0803bda60d0e9509a69fe3f6479b0b1ee
amit <shettya@gmail.com>
2017-02-12 07:34:38 -0800

    create database connectivity

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,15 @@
-module.exports = function(app){
+module.exports = function(app,db){
 
-	app.get('/', function(req,res){
-
-		res.sendFile(process.cwd() + '/public/index.html');
-
-	});
+	app.route('/')
+		.get(function(req,res){
+			res.sendFile(process.cwd() + '/public/index.html');	
+			db.collection('test').insert({
+				name: 'Foo Bar',
+				age: 25
+			}, function(err){
+				if(err){
+					throw new Error("Error on inserting");
+				}
+			});
+		});
 }
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 'use strict';
 
 let express = require('express');
+let mongo = require('mongodb').MongoClient;
 let routes = require(__dirname + '/app/routes/index.js');
+
 let app = express();
 
-app.set('port', (process.env.PORT || 3000));
-app.use('/css', express.static(__dirname + '/public/css'));
+let mongoURL = 'mongodb://localhost:27017/urlshortener';
 
-routes(app);
+mongo.connect(mongoURL, function(err,db){
 
-app.listen(app.get('port'), function(){
-	console.log(`Now listening on port ${app.get('port')}`);
+	if(err){
+		throw new Error('Failed to connect to database...');
+	}else{
+		console.log('Successfully connected to databse on port 27017');
+	}
+
+	app.set('port', (process.env.PORT || 3000));
+	app.use('/css', express.static(__dirname + '/public/css'));
+
+	routes(app,db);
+
+	app.listen(app.get('port'), function(){
+		console.log(`Now listening on port ${app.get('port')}`);
+	});
+
 });
