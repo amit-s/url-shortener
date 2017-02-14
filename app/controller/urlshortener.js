f46abbb8d6cb040f6174880bb71adaff960b7cc0
amit <shettya@gmail.com>
2017-02-14 12:18:40 -0800

    clean up code

--- app/controller/urlshortener.js
+++ app/controller/urlshortener.js
@@ -5,39 +5,39 @@ exports.handleURL = function(req,res,next){
 	let db = req.app.db;
 
 	let cursor = db.collection('links').find({});
-			let projection = {_id: 0, original_url: 1, short_url: 1, short_path:1};			
-			cursor.project(projection);
+	let projection = {_id: 0, original_url: 1, short_url: 1, short_path:1};			
+	cursor.project(projection);
+	
+	cursor.toArray(function(err,docs){
+		if(err) throw err;
+
+		let original_url_list = docs.map(function(doc){
+			return doc.original_url;
+		});
+
+		let short_path_list = docs.map(function(doc){
+			return doc.short_path;
+		});
+
+		let existing_link_position = original_url_list.indexOf(original_url);
+
+
+		if((docs.length<1)||(existing_link_position == -1)){
+			let short_path = createShortPath(short_path_list);					
+			let short_url = `${hostname}/${short_path}`;
 			
-			cursor.toArray(function(err,docs){
-				if(err) throw err;
-
-				let original_url_list = docs.map(function(doc){
-					return doc.original_url;
-				});
-
-				let short_path_list = docs.map(function(doc){
-					return doc.short_path;
-				});
-
-				let existing_link_position = original_url_list.indexOf(original_url);
-
-
-				if((docs.length<1)||(existing_link_position == -1)){
-					let short_path = createShortPath(short_path_list);					
-					let short_url = `${hostname}/${short_path}`;
-					
-					
-					db.collection('links').insert({original_url, short_url, short_path},function(err){
-						if (err) throw err;
-						console.log(`new url ${original_url} added to DB`);
-						res.json({original_url, short_url});
-					});
-				}else{
-				console.log('here');
-				let doc = docs[existing_link_position];				
-				res.json({original_url: doc.original_url, short_url: doc.short_url});
-				}				
-			});			
+			
+			db.collection('links').insert({original_url, short_url, short_path},function(err){
+				if (err) throw err;
+				console.log(`new url ${original_url} added to DB`);
+				res.json({original_url, short_url});
+			});
+		}else{
+		console.log('here');
+		let doc = docs[existing_link_position];				
+		res.json({original_url: doc.original_url, short_url: doc.short_url});
+		}				
+	});			
 }
 
 createShortPath =  function(list){
@@ -52,9 +52,3 @@ exports.validateURL =  function (url){
 	let urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 	return urlRegex.test(url);
 }
-
-
-
-
-
-
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,4 @@
-//let handleURL = require(process.cwd() + '/app/controller/urlshortener.js');
-
 let api = require(process.cwd() + '/app/controller/urlshortener.js');
-
 
 module.exports = function(app,db){
 
@@ -11,11 +8,6 @@ module.exports = function(app,db){
 			res.render('index.pug');
 
 		});
-
-	/*app.route('/*')
-		.get(function(req,res){
-			console.log(req.params);
-		});*/
 
 	app.route('/new/*')
 		.get(function(req,res,next){
@@ -27,74 +19,5 @@ module.exports = function(app,db){
 			}
 
 			api.handleURL(req,res,next);			
-			
 		});
 }
-
-
-/*function validateURL(url){	
-	let urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-	return urlRegex.test(url);
-}*/
-
-
-
-/*
-
-
-function handleURL(req,res,next){
-
-	let original_url = req.params[0];
-	let hostname = req.headers.host;
-	let db = req.app.db;
-
-	let cursor = db.collection('links').find({});
-			let projection = {_id: 0, original_url: 1, short_url: 1, short_path:1};			
-			cursor.project(projection);
-			
-			cursor.toArray(function(err,docs){
-				if(err) throw err;
-
-				let original_url_list = docs.map(function(doc){
-					return doc.original_url;
-				});
-
-				let short_path_list = docs.map(function(doc){
-					return doc.short_path;
-				});
-
-				let existing_link_position = original_url_list.indexOf(original_url);
-
-
-				if((docs.length<1)||(existing_link_position == -1)){
-					let short_path = createShortPath(short_path_list);					
-					let short_url = `${hostname}/${short_path}`;
-					
-					
-					db.collection('links').insert({original_url, short_url, short_path},function(err){
-						if (err) throw err;
-						console.log(`new url ${original_url} added to DB`);
-						res.json({original_url, short_url});
-					});
-				}else{
-				console.log('here');
-				let doc = docs[existing_link_position];				
-				res.json({original_url: doc.original_url, short_url: doc.short_url});
-				}				
-			});			
-}
-
-
-function createShortPath(list){
-	let short = 1234;
-	while(list.indexOf(short) != -1){
-		short = Math.floor(Math.random()*8999 + 1000);		
-	}
-	return short;	
-}
-
-function validateURL(url){	
-	let urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-	return urlRegex.test(url);
-}
-*/
